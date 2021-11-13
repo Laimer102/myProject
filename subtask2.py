8063b88795b4f1b3eb4aee7419cfd85199c78031
Laimer102 <94228169+Laimer102@users.noreply.github.com>
2021-11-13 23:29:31 +0100

    Create subtask2.py

---
+++ subtask2.py
@@ -0,0 +1,26 @@
+#The program reads in a sequence of natural numbers and computes the number of entries, the sum, the average and the minimum.
+x=input("Input:")
+x=int(x)
+n=0
+s=0
+a=0
+m=x
+while(x!=-1):
+    n=n+1
+    s=s+x
+    if(m>x):
+        m=x
+    x=input("Input:")
+    x=int(x)
+if(n==0):
+    a=-1
+    m=-1
+else:
+    a=s/n
+print('Number of Entries:') 
+print(n)
+print('Sum:')
+print(s)
+print('Average:')
+print(a)
+print('Minimum:')
